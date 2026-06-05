77af4293bd8d84479c7bec858f57402c1c7eb2cf
molakathallaseshareddy <mksreddymseshareddy@gmail.com>
2026-06-05 17:40:00 +0530

    code is changing in Recursion_Factorial

--- Recursion/Factorial_of_a_given_number_using_Recursion.py
+++ Recursion/Factorial_of_a_given_number_using_Recursion.py
@@ -3,6 +3,7 @@ class Solution:
         if n == 0:
             return 1
         return n * self.factorial(n-1)
+        
 if __name__ == "__main__":
     s = Solution()
     result = s.factorial(10)
