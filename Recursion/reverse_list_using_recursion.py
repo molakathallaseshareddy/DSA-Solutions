a84259d79cf5529dd64e2e980c8460ab4794779b
molakathallaseshareddy <mksreddymseshareddy@gmail.com>
2026-06-15 17:57:22 +0530

    reverse list using recursion

---
+++ Recursion/reverse_list_using_recursion.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverse(self, arr: list, n: int) -> None:
+        if n==0:
+            return []
+        return [arr[n-1]]+self.reverse(arr, n-1)
+if __name__ == "__main__":
+    s= Solution()
+    print(s.reverse([1,2,3,4,5],5))
