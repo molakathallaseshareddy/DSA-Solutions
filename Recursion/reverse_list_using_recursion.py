1768673ba19d37f195c2a41c8895435edcab2f6d
molakathallaseshareddy <mksreddymseshareddy@gmail.com>
2026-06-19 16:35:08 +0530

    list recursion, second solution added

--- Recursion/reverse_list_using_recursion.py
+++ Recursion/reverse_list_using_recursion.py
@@ -1,3 +1,4 @@
+#First Solution
 class Solution:
     def reverse(self, arr: list, n: int) -> None:
         if n==1:
@@ -6,3 +7,17 @@ class Solution:
 if __name__ == "__main__":
     s= Solution()
     print(s.reverse([1,2,3,4,5],5))
+
+
+#Second Solution
+class Solution:
+    def reverse(self, arr: list, n: int) -> None:
+        def helper(left, right):
+            if left >= right:
+                return
+
+            arr[left], arr[right] = arr[right], arr[left]
+
+            helper(left + 1, right - 1)
+
+        helper(0, n - 1)
