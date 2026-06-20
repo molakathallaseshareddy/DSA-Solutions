17310a6fa6f02dbe5a3d636c1ac14c64e1ab7021
molakathallaseshareddy <mksreddymseshareddy@gmail.com>
2026-06-20 22:21:28 +0530

    added new code for palindrome code using recursion

--- Recursion/string_recurion_palindrome.py
+++ Recursion/string_recurion_palindrome.py
@@ -1,9 +1,22 @@
+# class Solution:    
+#     def palindromeCheck(self, s, n:int):
+#         if n ==0:
+#             return s[n]
+#         p = s[n-1]+self.palindromeCheck(s, n-1)
+#         if p == s:
+#             return True
+#         else:
+#             return False
+
+
+# Second Solution
 class Solution:    
-    def palindromeCheck(self, s, n:int):
-        if n ==0:
-            return s[n]
-        p = s[n-1]+self.palindromeCheck(s, n-1)
-        if p == s:
-            return True
-        else:
-            return False
+    def palindromeCheck(self, s):
+        #your code goes here
+        n = len(s)
+        def help(n):
+            if n == 1:
+                return s[n-1]
+            return s[n-1] + help(n-1)
+        a = help(n)
+        return a
