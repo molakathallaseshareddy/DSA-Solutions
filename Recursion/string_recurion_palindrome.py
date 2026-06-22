f2eb7320c9f1d6fcb04847478eecc9f8357bc959
molakathallaseshareddy <mksreddymseshareddy@gmail.com>
2026-06-22 14:59:45 +0530

    This is leetcode solution for palindrome check

--- Recursion/string_recurion_palindrome.py
+++ Recursion/string_recurion_palindrome.py
@@ -28,3 +28,22 @@ if __name__ == "__main__":
     s = Solution()
     result = s.palindromeCheck("madam")
     print(result)
+
+#Third Solution
+#This is leetcode solution for palindrome check
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        a = ""
+        for i in s:
+            if ("A" <= i <= "Z") or ("a" <= i <= "z") or ("0" <= i <= "9"):
+                a += i.lower()
+        n = len(a)
+        def help(n):
+            if n ==0:
+                return ""
+            return a[n-1] + help(n-1)
+        b = help(n)
+        if a == b:
+            return True
+        else:
+            return False
