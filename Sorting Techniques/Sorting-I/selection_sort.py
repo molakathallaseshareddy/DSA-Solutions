4bc72734a50615df32f0201b91a4399a50410805
molakathallaseshareddy <mksreddymseshareddy@gmail.com>
2026-06-25 10:42:30 +0530

    Selection sort solution 1

---
+++ Sorting Techniques/Sorting-I/selection_sort.py
@@ -0,0 +1,9 @@
+#Solution 1
+class Solution:
+    def selectionSort(self, nums:list):
+        result = sorted(nums)
+        return result
+s = Solution()
+res =s.selectionSort([3,5,1,2,4])
+print(res)
+
