424fdc0a395bedd5ee2e183f57addbb5b8545596
molakathallaseshareddy <mksreddymseshareddy@gmail.com>
2026-06-26 10:54:26 +0530

    selection sort solution 3

--- Sorting Techniques/Sorting-I/selection_sort.py
+++ Sorting Techniques/Sorting-I/selection_sort.py
@@ -15,3 +15,25 @@ class Solution:
 s = Solution()
 res =s.selectionSort([3,5,1,2,4])
 print(res)
+
+#Solution 3
+class Solution:
+    def selectionSort(self, nums):
+        result = []
+        def help(nums):
+            a = nums[0]
+            for i in nums:
+                if a>i:
+                    a = i
+                    continue
+            result.append(a)
+            nums.remove(a)
+            if len(nums) == 0:
+                return
+            help(nums)
+        help(nums)
+        return result
+if __name__ == "__main__":
+    s = Solution()
+    print(s.selectionSort([4,5,2,3,1]))
+            
