d78620a925b94db2900cafd63951ad8dea28dc48
molakathallaseshareddy <mksreddymseshareddy@gmail.com>
2026-06-27 16:57:29 +0530

    #Solution 5 reverse selection sort

--- Sorting Techniques/Sorting-I/selection_sort.py
+++ Sorting Techniques/Sorting-I/selection_sort.py
@@ -57,4 +57,22 @@ if __name__ == "__main__":
     s = Solution()
     print(s.selectionSort([2,4,1,3,6,5]))
             
-            
+#Solution 5 reverse selection sort
+class Solution:
+    def selectionSort(self, nums):
+        result =[]
+        def help():
+            if len(nums) == 0:
+                return
+            num = nums[0]
+            for i in nums:
+                if num < i:
+                    num = i
+            result.append(num)
+            nums.remove(num)
+            return help()
+        help()
+        return result
+if __name__ == "__main__":
+    s = Solution()
+    print(s.selectionSort([2,4,1,3,6,5]))
