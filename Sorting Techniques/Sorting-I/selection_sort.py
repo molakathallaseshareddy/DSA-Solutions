dce76e916d336146751d5cdd6ccb362b71cbf6eb
molakathallaseshareddy <mksreddymseshareddy@gmail.com>
2026-06-27 16:55:56 +0530

    added solution 4 for Selection Sort

--- Sorting Techniques/Sorting-I/selection_sort.py
+++ Sorting Techniques/Sorting-I/selection_sort.py
@@ -36,4 +36,25 @@ class Solution:
 if __name__ == "__main__":
     s = Solution()
     print(s.selectionSort([4,5,2,3,1]))
+
+#Solution 4
+class Solution:
+    def selectionSort(self, nums):
+        result =[]
+        def help():
+            if len(nums) == 0:
+                return
+            num = nums[0]
+            for i in nums:
+                if num > i:
+                    num = i
+            result.append(num)
+            nums.remove(num)
+            return help()
+        help()
+        return result
+if __name__ == "__main__":
+    s = Solution()
+    print(s.selectionSort([2,4,1,3,6,5]))
+            
             
