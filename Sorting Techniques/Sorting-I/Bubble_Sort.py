4486b3c32fe2018bc41efd0b668b0c553add49d8
molakathallaseshareddy <mksreddymseshareddy@gmail.com>
2026-06-30 10:38:04 +0530

    Bubble sort code is updated

--- Sorting Techniques/Sorting-I/Bubble_Sort.py
+++ Sorting Techniques/Sorting-I/Bubble_Sort.py
@@ -1,4 +1,10 @@
-#Bubble Sort
 class Solution:
     def bubbleSort(self, nums:list):
-        result = []
+        for i in range(len(nums)):
+            for j in range(len(nums)-i-1):
+                if nums[j] > nums[j+1]:
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+        return nums
+if __name__ == "__main__":
+    s = Solution()
+    print(s.bubbleSort([3,2,7,5,1]))
