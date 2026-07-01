a3864da3585f4466128efecce00e7f3766c6c37c
molakathallaseshareddy <mksreddymseshareddy@gmail.com>
2026-07-01 16:47:21 +0530

    updated insertion sort code-1

--- Sorting Techniques/Sorting-I/insertion_sort.py
+++ Sorting Techniques/Sorting-I/insertion_sort.py
@@ -1,15 +1,14 @@
 class Solution:
     def insertionSort(self, nums):
         n = len(nums)
-
         for i in range(1, n):
             key = nums[i]
             j = i - 1
-
             while j >= 0 and nums[j] > key:
                 nums[j + 1] = nums[j]
                 j -= 1
-
             nums[j + 1] = key
-
         return nums
+if __name__ == "__main__":
+    s = Solution()
+    print(s.insertionSort([3,2,7,5,1]))
