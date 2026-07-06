cd8158c61b6253cc1037c81f186abfb47a13139b
molakathallaseshareddy <mksreddymseshareddy@gmail.com>
2026-07-06 15:10:16 +0530

    Largest Eliment in Array

---
+++ Arrays/Easy/Largest_Eliment.py
@@ -0,0 +1,11 @@
+class Solution:
+    def largestElement(self, nums):
+        value = nums[0]
+        for i in nums:
+            if i > value:
+                value = i
+        return value
+if __name__ == "__main__":
+    s = Solution()
+    result = s.largestElement([3, 3, 0, 99, -40])
+    print(result)
