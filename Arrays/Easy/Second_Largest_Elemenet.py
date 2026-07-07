d62a959812abdc1c030cece52456c5468f5a89b2
molakathallaseshareddy <mksreddymseshareddy@gmail.com>
2026-07-07 13:17:21 +0530

    Second Largest ELement code is updated

--- Arrays/Easy/Second_Largest_Elemenet.py
+++ Arrays/Easy/Second_Largest_Elemenet.py
@@ -2,7 +2,11 @@ class Solution:
     def secondLargestElement(self, nums):
         nums = list(set(nums))
         nums.sort()
+        if len(nums) == 1 or len(nums) == 0:
+            return -1
         result = nums[-2]
         return result
 
-print(Solution().secondLargestElement([1, 2, 3, 4, 5]))
+if __name__ == "__main__":
+    nums = [1, 2, 3, 4, 5]
+    print(Solution().secondLargestElement(nums))
