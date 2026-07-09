da5ca9522f9b1a2ab7f1022331482396f331e915
molakathallaseshareddy <mksreddymseshareddy@gmail.com>
2026-07-09 23:31:22 +0530

    code updated

--- Arrays/Easy/Largest_Element.py
+++ Arrays/Easy/Largest_Element.py
@@ -1,3 +1,15 @@
+# class Solution:
+#     def largestElement(self, nums):
+#         value = nums[0]
+#         for i in nums:
+#             if i > value:
+#                 value = i
+#         return value
+# if __name__ == "__main__":
+#     s = Solution()
+#     result = s.largestElement([3, 3, 0, 99, -40])
+#     print(result)
+
 class Solution:
     def largestElement(self, nums):
         value = nums[0]
