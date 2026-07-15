e07ec5b39cb2e84fd3571d4929c5af2de7b67050
molakathallaseshareddy <mksreddymseshareddy@gmail.com>
2026-07-15 18:11:05 +0530

    added new solution in left rotate array

--- Arrays/Easy/Left_Rotate_Array_by_One.py
+++ Arrays/Easy/Left_Rotate_Array_by_One.py
@@ -1,13 +1,21 @@
-#Left Rotate Array by One
+# #Left Rotate Array by One
+# class Solution:
+#     def rotateArrayByOne(self, nums):
+#         value = nums[0]
+#         nums.pop(0)
+#         nums.append(value)
+#         return nums
+
+# if __name__ == "__main__":
+#     s = Solution()
+#     result = s.leftRotateArrayByOne([1, 2, 3, 4, 5])
+#     print(result)
+
+# Left Rotate Array by One
 class Solution:
     def rotateArrayByOne(self, nums):
-        value = nums[0]
-        nums.pop(0)
-        nums.append(value)
+        first_element = nums[0]
+        for i in range(len(nums) - 1):
+            nums[i] = nums[i + 1]
+        nums[-1] = first_element
         return nums
-
-if __name__ == "__main__":
-    s = Solution()
-    result = s.leftRotateArrayByOne([1, 2, 3, 4, 5])
-    print(result)
-    
