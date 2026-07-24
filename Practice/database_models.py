9f0740fc321647fdbdea08f00cc637e7d7dd2718
molakathallaseshareddy <mksreddymseshareddy@gmail.com>
2026-07-24 16:11:54 +0530

    remove the product

--- Practice/database_models.py
+++ Practice/database_models.py
@@ -3,13 +3,6 @@ from sqlalchemy import Column, Integer, Float, String
 
 Base = declarative_base()
 
-class Product(Base):
-    __tablename__ = "product"
-    rollno = Column(Integer, primary_key = True)
-    stname = Column(String(50))
-    Class = Column(Integer)
-    fee = Column(Float)
-
 class Student(Base):
     __tablename__ = "student"
     rollno = Column(Integer, primary_key = True)
