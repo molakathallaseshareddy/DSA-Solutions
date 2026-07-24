f159241daf13785c0165a9f7bba6d955edbc126e
molakathallaseshareddy <mksreddymseshareddy@gmail.com>
2026-07-24 16:09:33 +0530

    added database models

--- Practice/database_models.py
+++ Practice/database_models.py
@@ -9,3 +9,10 @@ class Product(Base):
     stname = Column(String(50))
     Class = Column(Integer)
     fee = Column(Float)
+
+class Student(Base):
+    __tablename__ = "student"
+    rollno = Column(Integer, primary_key = True)
+    stname = Column(String(50))
+    Class = Column(Integer)
+    fee = Column(Float)
